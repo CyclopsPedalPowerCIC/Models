8c8c41b050e4dd749ad5a142a9867b7b85a9c76e
Tom Hargreaves <hex@sphere.chronosempire.org.uk>
2018-10-15 23:15:58 +0100

    websocketed city lights and switches

--- Arduino/ESP8266/CityLightsV2/CityLightsV2.ino/CityLightsV2.ino.ino
+++ Arduino/ESP8266/CityLightsV2/CityLightsV2.ino/CityLightsV2.ino.ino
@@ -1,136 +1,237 @@
-//#define FASTLED_ESP8266_NODEMCU_PIN_ORDER //this appears to be its default behaviour
 #define FASTLED_ESP8266_RAW_PIN_ORDER  //needed to stop the fastLED library being too clever for its own good and assuming D2->GPOI19->D19
 #define FASTLED_ALLOW_INTERRUPTS 0
 #include "FastLED.h"
-#include <ShiftRegister74HC595.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <WebSocketsServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
 #define Version F("City LIghts 08/10/18")
-
+#define NLEDS 28
 
 const char* ssid = "Cyclops_Wifi";
 const char* password = "skullface";
 
 ESP8266WebServer server(80);
-
+WebSocketsServer webSocket = WebSocketsServer(81);
 
 // How many leds in your strip?
 #define NUM_LEDS 600   //more than needed
-ShiftRegister74HC595 sr (24, D0, D2, D1); 
+
+#define PIN_DATA595   D0
+#define PIN_CLK595    D2
+#define PIN_LATCH595  D1
+#define PIN_SWITCH    D3
+#define PIN_LEDS      D4
 //#define buttonePin D1
+//serial=D0, clock=D2, latch=D1
 
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-#define DATA_PIN D4
 //#define CLOCK_PIN 13
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
-//int switchMapR[28]={24,23,30,31,0,1,7,9,8,27,28,26,25,29,21,19,22,18,20,12,11,13,10,4,5,6,3,2};  //lights->switches, wrong way round
+//int switchMap[28]={24,23,30,31,0,1,7,9,8,27,28,26,25,29,21,19,22,18,20,12,11,13,10,4,5,6,3,2};  //lights->switches, wrong way round
 int switchMap[32]={4,5,27,26,23,24,25,6,8,7,22,20,19,21,28,28,28,28,17,15,18,14,16,1,0,12,11,9,10,13,2,3};  //switches->lights
 int rfidMap[28]={13,9,12,11,10,17,14,18,15,16,3,0,1,2,6,7,4,5,8,25,27,24,26,23,21,19,22,20}; //rfid readers->lights
 
-int switchState[29]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //29th value is a dummy which corresponds to non existant switches
+uint8_t switchState[28]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
-void setup() { 
-  Serial.begin(9600);
-
-
-      ESP.eraseConfig();
-        WiFi.begin(ssid, password);
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(250);
-    digitalWrite(LED_BUILTIN, LOW);
-
-    Serial.print(".");
-    //setdisplay();
+void init_wifi() {
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  Serial.printf("MAC address: %02x:%02x:%02x:%02x:%02x:%02x\r\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  WiFi.mode(WIFI_STA);
+  for (;;) {
+    if (connect_wifi("Cyclops_Wifi", "skullface")) return;
+    //if (connect_wifi("LeedsHackspace", "blinkyLED")) return;
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+}
 
-  if (MDNS.begin("esp8266")) {
+bool connect_wifi(const char* ssid, const char* password) {
+  Serial.printf("Wifi: trying %s/%s", ssid,password);
+  WiFi.begin(ssid, password);
+  int tries = 80;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (!--tries) {
+      Serial.print("\r\nfailed\r\n");
+      return false;
+    }
+  }
+  Serial.printf("\r\ngot IP ");
+  Serial.println(WiFi.localIP());
+  return true;
+}
+
+void init_server() {
+  init_wifi();
+  if (MDNS.begin("citymaster")) {
     Serial.println("MDNS responder started");
   }
-      server.on("/", handleRoot);
-    server.on("/setBlock", handleBlock);
-    server.on("/setCorner", handleCorner);
-    server.on("/getSwitches", handleSwitch);
-      server.onNotFound(handleNotFound);
-  server.begin();
-      FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-      //FastLED.setBrightness(127);
 
-      pinMode(LED_BUILTIN, OUTPUT);
-      pinMode(D0, OUTPUT); 
-      pinMode(D1, OUTPUT); 
-      pinMode(D2, OUTPUT); 
-      pinMode(D3, INPUT);
-      digitalWrite(D3,0); 
-      pinMode(D4, OUTPUT); 
+  server.on("/", handleRoot);
+  server.on("/setBlock", handleBlock);
+  server.on("/setCorner", handleCorner);
+  server.on("/getSwitches", handleSwitch);
+
+  server.begin();
+  Serial.println("HTTP server started");
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  Serial.println("WS server started");
+}
+
+void setup() { 
+  Serial.begin(115200);
+  Serial.print("\r\n\r\n\r\ncitylights (" __TIME__ " " __DATE__ ")\r\n");
+  init_server();
+
+  FastLED.addLeds<WS2812B, PIN_LEDS, GRB>(leds, NUM_LEDS);
+  //FastLED.setBrightness(127);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_LATCH595, OUTPUT); 
+  pinMode(PIN_CLK595, OUTPUT); 
+  pinMode(PIN_DATA595, OUTPUT); 
+  pinMode(PIN_SWITCH, INPUT);
+  digitalWrite(PIN_SWITCH,0);
+  pinMode(PIN_LEDS, OUTPUT); 
 
   for (int i=0;i<28;i++){
-    
     setBlock(i,CRGB::Green);
     delay(40);
-    checkSwitches();
     FastLED.show();
-    }
-    for (int i=0;i<28;i++){
-    
-    setBlock(i,CRGB::Green);
-    delay(40);
-    checkSwitches();
-    FastLED.show();
-    } 
-      
+  } 
 }
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+  case WStype_DISCONNECTED:
+    Serial.printf("[%u] Disconnected!\n", num);
+    break;
+  case WStype_CONNECTED:
+    {
+      IPAddress ip = webSocket.remoteIP(num);
+      Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+      webSocket.sendTXT(num, message_string());
+    }
+    break;
+  case WStype_BIN:
+  case WStype_TEXT:
+    Serial.printf("type %d len %d\r\n", type, length);
+    if (length == NLEDS*3) {
+      set_leds(payload);
+    }
+    webSocket.sendTXT(num, message_string());
+    break;
+  }
+}
+
+void set_leds(uint8_t *buf) {
+  Serial.print("set_leds\r\n");
+  for (int i=0; i<NLEDS; i++, buf+=3) {
+    setBlock(i,CRGB(buf[0],buf[1],buf[2]));
+  }
+  FastLED.show();
+}
+
+void write595(const uint8_t *arr, uint8_t len) {
+  while (len--)
+    shiftOut(PIN_DATA595, PIN_CLK595, MSBFIRST, arr[len]);
+  digitalWrite(PIN_LATCH595, HIGH); 
+  digitalWrite(PIN_LATCH595, LOW); 
+}
+
+static inline void set_latch(uint8_t *arr, uint8_t bitno, bool val) {
+  uint8_t mask = 1<<(bitno&7);
+  arr[bitno>>3] &= ~mask;
+  arr[bitno>>3] |= val?mask:0;
+}
+
+unsigned long lastelapsed;
 
 void loop() { 
-
-  checkSwitches();
+  unsigned long t1, t2;
+  t1 = millis();
+  poll_switches();
   server.handleClient();
-  
+  webSocket.loop();
+  if (WiFi.status() != WL_CONNECTED) {
+    init_wifi();
+  }
+  t2 = millis();
+  lastelapsed = t2-t1;
+  Serial.printf("elapsed %d  \r", lastelapsed);
 }
 
-void checkSwitches(){
-  
-  for (int i=0;i<32;i++){
-    
-    sr.set(3,i%2);  //S0
-    sr.set(2,(i/2)%2); //S1
-    sr.set(1,(i/4)%2); //S2
-    sr.set(0,(i/8)%2); //S3
-    sr.set(4,(i/16)%2); //En1
-    sr.set(5,(i/16+1)%2); //En2
-    //delay(1);
-    if (!digitalRead(D3)){
-      Serial.print(i);
-      
-    setCorner(switchMap[i],CRGB::Yellow);
-    switchState[switchMap[i]]=1;  //records if the switch has been pressed
-      }
-    else {
-      //Serial.print(" ");
-      
-    setCorner(switchMap[i],CRGB::White);
-      }
-    
-    }
-    //Serial.println("");
+uint8_t shiftreg[3] = { 0 };
 
-  FastLED.show();
+uint32_t switches = 0;
+#define NTRIES 5
+
+uint32_t read_switches() {
+  static uint8_t debounce[32] = { 0 };
+  uint32_t pressed = 0;
   
+  for (int i=0; i<32; i++) {
+    set_latch(shiftreg, 3, i&1);
+    set_latch(shiftreg, 2, i&2);
+    set_latch(shiftreg, 1, i&4);
+    set_latch(shiftreg, 0, i&8);
+    set_latch(shiftreg, 4, i&16);
+    set_latch(shiftreg, 5, !(i&16));
+    write595(shiftreg, sizeof shiftreg);
+    bool r = !digitalRead(D3);
+
+    if (((switches>>i) ^ r)&1) {
+      if (++debounce[i] > NTRIES) {
+	debounce[i] = 0;
+	//Serial.printf("%d changed to %d\r\n", i, r);
+	switches = (switches & ~(1<<i)) | (r<<i);
+	setCorner(switchMap[i],r?CRGB::Yellow:CRGB::White);
+	pressed |= (1<<i);
+      }
+    } else {
+      debounce[i] = 0;
+    }
   }
+  //Serial.printf("%08x %08x %08x\r\n", switches, pressed);
+  return pressed;
+}
+
+void poll_switches(void) {
+  uint32_t changed = read_switches(), ch = changed;
+  uint32_t pressed = changed & switches;
+  if (changed) {
+    while (uint8_t b = ffs(ch)) {
+      uint32_t mask = 1<<(b-1);
+      if (pressed & mask)
+	Serial.printf("%d pressed\r\n", b);
+      else
+	Serial.printf("%d released\r\n", b);
+      ch &= ~mask;
+    }
+  
+    webSocket.broadcastTXT(message_string());
+    FastLED.show();
+  }
+}
+
+char *message_string() {
+  static char buf[16];
+  snprintf(buf, sizeof buf, "%d", switches);
+  return buf;
+}
+
+CRGB parse_colour(const char **text) {
+  int r,g,b;
+  sscanf(*text, "%02x%02x%02x", &r, &g, &b);
+}
 
 void handleBlock(){
 
@@ -141,29 +242,28 @@ void handleBlock(){
     if (server.argName(i)=="number"){
       Serial.print("number");
       bNumber=server.arg(i).toInt();
-      } 
+    } 
     if (server.argName(i)=="red"){
       Serial.print("red");
       bColour.r=server.arg(i).toInt();
-      }
+    }
     if (server.argName(i)=="green"){
       Serial.print("green");
       bColour.g=server.arg(i).toInt();
-      }
+    }
     if (server.argName(i)=="blue"){
       Serial.print("blue");
       bColour.b=server.arg(i).toInt();
-      }  
+    }  
 
   }
   Serial.print("handleBlock middle");
   if ((bNumber>-1)&&(bNumber<28)){
     setBlock(bNumber,bColour);
-    
+
     FastLED.show();
-    server.sendHeader("Access-Control-Allow-Origin", "*");
     server.send(200, "text/plain", "Set");
-    }
+  }
   else {  server.send(200, "text/plain", "Failed");}
 
   Serial.print("handleBlock end");
@@ -171,103 +271,73 @@ void handleBlock(){
 
 
 void handleRoot() {
-  //digitalWrite(led, 1);
-  server.sendHeader("Access-Control-Allow-Origin", "*");//should be included automatically, acording to documentation, but isn't for some reason
-  server.send(200, "text/plain", String("Cyclops ESP Version: ")+String(Version));
-  //digitalWrite(led, 0);
+  server.send(200, "text/plain", message_string());
 }
 
 
 void handleCorner(){
 
   int bNumber=-1;
-  CRGB bColour=CRGB::Black;
+  CRGB bColour;
   //Serial.print("handleBlock called");
   for (uint8_t i=0; i<server.args(); i++){
     if (server.argName(i)=="number"){
-    //  Serial.print("number");
+      //  Serial.print("number");
       bNumber=server.arg(i).toInt();
-      } 
+    } 
     if (server.argName(i)=="red"){
       //Serial.print("red");
       bColour.r=server.arg(i).toInt();
-      }
+    }
     if (server.argName(i)=="green"){
       //Serial.print("green");
       bColour.g=server.arg(i).toInt();
-      }
+    }
     if (server.argName(i)=="blue"){
       //Serial.print("blue");
       bColour.b=server.arg(i).toInt();
-      }  
+    }  
 
   }
-//  Serial.print("handleBlock middle");
+  //  Serial.print("handleBlock middle");
   if ((bNumber>-1)&&(bNumber<28)){
     setCorner(bNumber,bColour);
-    
+
     FastLED.show();
-    server.sendHeader("Access-Control-Allow-Origin", "*");//should be included automatically, acording to documentation, but isn't for some reason
     server.send(200, "text/plain", "Set");
-    }
+  }
   else {  server.send(200, "text/plain", "Failed");}
 
 
 }
 
 void setBlock(int number, CRGB colour){
-  if (number>27){return;}
-  if (number<0){return;}
-int start=19*number;
-for (int i=0;i<15;i++){
-  
-  leds[i+2+start]=colour;
+
+  int start=19*number;
+  for (int i=0;i<15;i++){
+
+    leds[i+2+start]=colour;
   }
-  
-  }
+
+}
 
 
 void setCorner(int number, CRGB colour){
-    if (number>27){return;}
-  if (number<0){return;}
-int start=19*number;
+  int start=19*number;
 
-for (int i=0;i<2;i++){
-  
-  leds[i+start]=colour;
-  leds[i+17+start]=colour;
+  for (int i=0;i<2;i++){
+
+    leds[i+start]=colour;
+    leds[i+17+start]=colour;
   }
-  
-  }
+
+}
 
 void handleSwitch(){
 
   String returnString="";
-  for (int i=0;i<32;i++){
-    returnString+=switchState[i];
-    switchState[i]=0; //reset switches    
-    }
-    server.sendHeader("Access-Control-Allow-Origin", "*");
+  for (int i=0;i<28;i++){
+    returnString+=switchState[i];    
+  }
   server.send(200, "text/plain", returnString);
-  }
-
-
-void handleNotFound(){
-//  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET)?"GET":"POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i=0; i<server.args(); i++){
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.sendHeader("Access-Control-Allow-Origin", "*");//should be included automatically, acording to documentation, but isn't for some reason
-  server.send(404, "text/plain", message);
-  //digitalWrite(led, 0);
 }
-
-  
