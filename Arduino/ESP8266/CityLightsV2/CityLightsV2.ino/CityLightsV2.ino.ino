7fed9feff937acf138a46ddba9326632a26cc9d8
Tom Hargreaves <hex@sphere.chronosempire.org.uk>
2018-10-27 00:30:27 +0100

    animation support, gamma correction, fix getSwitches, cleanups

--- Arduino/ESP8266/CityLightsV2/CityLightsV2.ino/CityLightsV2.ino.ino
+++ Arduino/ESP8266/CityLightsV2/CityLightsV2.ino/CityLightsV2.ino.ino
@@ -7,16 +7,11 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-#define Version F("City LIghts 08/10/18")
-#define NLEDS 28
-
-const char* ssid = "Cyclops_Wifi";
-const char* password = "skullface";
+#define NSLOTS 28
 
 ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-// How many leds in your strip?
 #define NUM_LEDS 600   //more than needed
 
 #define PIN_DATA595   D0
@@ -24,21 +19,40 @@ WebSocketsServer webSocket = WebSocketsServer(81);
 #define PIN_LATCH595  D1
 #define PIN_SWITCH    D3
 #define PIN_LEDS      D4
-//#define buttonePin D1
-//serial=D0, clock=D2, latch=D1
-
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
-//#define CLOCK_PIN 13
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
-//int switchMap[28]={24,23,30,31,0,1,7,9,8,27,28,26,25,29,21,19,22,18,20,12,11,13,10,4,5,6,3,2};  //lights->switches, wrong way round
+int revswitchMap[28]={24,23,30,31,0,1,7,9,8,27,28,26,25,29,21,19,22,18,20,12,11,13,10,4,5,6,3,2};  //lights->switches, wrong way round
 int switchMap[32]={4,5,27,26,23,24,25,6,8,7,22,20,19,21,28,28,28,28,17,15,18,14,16,1,0,12,11,9,10,13,2,3};  //switches->lights
 int rfidMap[28]={13,9,12,11,10,17,14,18,15,16,3,0,1,2,6,7,4,5,8,25,27,24,26,23,21,19,22,20}; //rfid readers->lights
 
-uint8_t switchState[28]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+uint32_t switches = 0;
+
+typedef enum { ANIM_NONE=0, ANIM_FLASH_SLOW, ANIM_FLASH_FAST,
+	       ANIM_CRAWL_SLOW_LEFT, ANIM_CRAWL_SLOW_RIGHT,
+	       ANIM_CRAWL_FAST_LEFT, ANIM_CRAWL_FAST_RIGHT,
+	       ANIM_DISCO
+} anim_t;
+
+bool animating = true;
+uint32_t frame;
+
+struct {
+  CRGB main_colour, main_flash_colour,
+    tip_colour, tip_highlight_colour;
+  anim_t anim_mode;
+  int frame;
+} slot[NSLOTS] = {
+  CRGB(0,0,0),
+  CRGB(255,255,255),
+  CRGB(0,0,0),
+  CRGB(0,0,0),
+  ANIM_NONE, //FLASH_SLOW,
+  0
+};
+
+// bytes per slot in ws payload
+#define SLOTSIZE (13)
 
 void init_wifi() {
   uint8_t mac[6];
@@ -91,6 +105,9 @@ void setup() {
   Serial.print("\r\n\r\n\r\ncitylights (" __TIME__ " " __DATE__ ")\r\n");
   init_server();
 
+  for (int i=1; i<NSLOTS; i++)
+    slot[i] = slot[0];
+  
   FastLED.addLeds<WS2812B, PIN_LEDS, GRB>(leds, NUM_LEDS);
   //FastLED.setBrightness(127);
 
@@ -102,7 +119,7 @@ void setup() {
   digitalWrite(PIN_SWITCH,0);
   pinMode(PIN_LEDS, OUTPUT); 
 
-  for (int i=0;i<28;i++){
+  for (int i=0;i<NSLOTS;i++){
     setBlock(i,CRGB::Green);
     delay(40);
     FastLED.show();
@@ -124,7 +141,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
   case WStype_BIN:
   case WStype_TEXT:
     Serial.printf("type %d len %d\r\n", type, length);
-    if (length == NLEDS*3) {
+    if (length == NSLOTS*SLOTSIZE) {
       set_leds(payload);
     }
     webSocket.sendTXT(num, message_string());
@@ -132,12 +149,129 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
   }
 }
 
-void set_leds(uint8_t *buf) {
-  Serial.print("set_leds\r\n");
-  for (int i=0; i<NLEDS; i++, buf+=3) {
-    setBlock(i,CRGB(buf[0],buf[1],buf[2]));
+#define GAMMA_R 2.5
+#define GAMMA_G 2.5
+#define GAMMA_B 2.5
+
+#define H(n) G(n),G(n+1),G(n+2),G(n+3),G(n+4),G(n+5),G(n+6),G(n+7),\
+    G(n+8),G(n+9),G(n+10),G(n+11),G(n+12),G(n+13),G(n+14),G(n+15)
+
+#define G(n) pow((n)/255.0,GAMMA_R)*255
+const uint8_t gammatab_r[256] = {
+    H(0),H(16),H(32),H(48),H(64),H(80),H(96),H(112),
+    H(128),H(144),H(160),H(176),H(192),H(208),H(224),H(240)
+},
+#define G(n) pow((n)/255.0,GAMMA_G)*255
+  gammatab_g[256] = {
+    H(0),H(16),H(32),H(48),H(64),H(80),H(96),H(112),
+    H(128),H(144),H(160),H(176),H(192),H(208),H(224),H(240)
+},
+#define G(n) pow((n)/255.0,GAMMA_B)*255
+  gammatab_b[256] = {
+    H(0),H(16),H(32),H(48),H(64),H(80),H(96),H(112),
+    H(128),H(144),H(160),H(176),H(192),H(208),H(224),H(240)
+  };
+#undef G
+#undef H
+#undef GAMMA
+
+CRGB gamma_apply(CRGB in) {
+  return CRGB(gammatab_r[in.r],
+	      gammatab_g[in.g],
+	      gammatab_b[in.b]);
+}
+
+#define BUFCOL(offs) \
+  CRGB(buf[(offs)],buf[(offs+1)],buf[(offs+2)])
+
+void update_leds() {
+  for (int i=0; i<NSLOTS; i++) {
+    uint32_t f = frame-slot[i].frame;
+    switch (slot[i].anim_mode) {
+    case ANIM_NONE:
+      setBlock(i,slot[i].main_colour);
+      break;
+    case ANIM_FLASH_SLOW:
+      setBlock(i,blend(slot[i].main_colour,
+		       slot[i].main_flash_colour,
+		       quadwave8 (f>>2)));
+      break;
+    case ANIM_FLASH_FAST:
+      setBlock(i,blend(slot[i].main_colour,
+		       slot[i].main_flash_colour,
+		       quadwave8 (f)));
+      break;
+    case ANIM_CRAWL_SLOW_LEFT:
+      set_crawl_from_default(i, (f<<2),
+		0b1111000011110000
+		);
+      break;
+    case ANIM_CRAWL_SLOW_RIGHT:
+      set_crawl_from_default(i, ~(f<<2),
+		0b11110000
+		);
+      break;
+    case ANIM_CRAWL_FAST_LEFT:
+      set_crawl_from_default(i, (f<<3),
+		0b111000
+		);
+      break;
+    case ANIM_CRAWL_FAST_RIGHT:
+      set_crawl_from_default(i, ~(f<<3),
+		0b1100
+		);
+    case ANIM_DISCO:
+      switch ((f>>9)&7) {
+      case 0: case 1:
+	setBlock(i,blend(CRGB::Black,
+			 CRGB::Blue,
+			 quadwave8 (f>>1)));
+	break;
+      case 2: case 3:
+	set_crawl(i, (f<<2), 0b111000000,
+		  CRGB::Black, CRGB::Yellow
+		  );
+	break;
+      case 4: case 5:
+	setBlock(i,blend(CRGB::Black,
+			 CRGB::Red,
+			 quadwave8 (f>>1)));
+	break;
+      case 6: case 7:
+	set_crawl(i, ~(f<<2), 0b111000000,
+		  CRGB::Black, CRGB::Green
+		  );
+	break;
+      default:
+	setBlock(i,CRGB::Black);
+      }
+      break;
+    }
+    
+    setCorner(i, ((switches >> revswitchMap[i])&1) ? slot[i].tip_highlight_colour :
+	      slot[i].tip_colour);
+
   }
   FastLED.show();
+}
+
+void set_leds(uint8_t *buf) {
+  Serial.print("set_leds\r\n");
+  animating = false;
+  for (int i=0; i<NSLOTS; i++, buf+=SLOTSIZE) {
+    anim_t lastanim = slot[i].anim_mode,
+      thisanim = (anim_t)buf[12];
+    slot[i].main_colour = BUFCOL(0);
+    slot[i].main_flash_colour = BUFCOL(3);
+    slot[i].tip_colour = BUFCOL(6);
+    slot[i].tip_highlight_colour = BUFCOL(9);
+    slot[i].anim_mode = thisanim;
+    if (thisanim) animating = true;
+    if (lastanim != slot[i].anim_mode) {
+      slot[i].frame = frame; // restart animation on mode change
+    }
+  }
+  update_leds();
 }
 
 void write595(const uint8_t *arr, uint8_t len) {
@@ -158,9 +292,12 @@ unsigned long lastelapsed;
 void loop() { 
   unsigned long t1, t2;
   t1 = millis();
+  frame = t1;
   poll_switches();
   server.handleClient();
   webSocket.loop();
+  if (animating)
+    update_leds();
   if (WiFi.status() != WL_CONNECTED) {
     init_wifi();
   }
@@ -171,7 +308,6 @@ void loop() {
 
 uint8_t shiftreg[3] = { 0 };
 
-uint32_t switches = 0;
 #define NTRIES 5
 
 uint32_t read_switches() {
@@ -193,7 +329,7 @@ uint32_t read_switches() {
 	debounce[i] = 0;
 	//Serial.printf("%d changed to %d\r\n", i, r);
 	switches = (switches & ~(1<<i)) | (r<<i);
-	setCorner(switchMap[i],r?CRGB::Yellow:CRGB::White);
+	int s=switchMap[i];
 	pressed |= (1<<i);
       }
     } else {
@@ -210,15 +346,18 @@ void poll_switches(void) {
   if (changed) {
     while (uint8_t b = ffs(ch)) {
       uint32_t mask = 1<<(b-1);
-      if (pressed & mask)
+      if (pressed & mask) {
 	Serial.printf("%d pressed\r\n", b);
+	//	slot[switchMap[b-1]].anim_mode =
+	//	  (anim_t)((slot[switchMap[b-1]].anim_mode+1)%8);
+      }
       else
 	Serial.printf("%d released\r\n", b);
       ch &= ~mask;
     }
   
     webSocket.broadcastTXT(message_string());
-    FastLED.show();
+    update_leds();
   }
 }
 
@@ -227,14 +366,13 @@ char *message_string() {
   snprintf(buf, sizeof buf, "%d", switches);
   return buf;
 }
-
+#if 0
 CRGB parse_colour(const char **text) {
   int r,g,b;
   sscanf(*text, "%02x%02x%02x", &r, &g, &b);
 }
-
+#endif
 void handleBlock(){
-
   int bNumber=-1;
   CRGB bColour;
   Serial.print("handleBlock called");
@@ -255,17 +393,14 @@ void handleBlock(){
       Serial.print("blue");
       bColour.b=server.arg(i).toInt();
     }  
-
   }
   Serial.print("handleBlock middle");
   if ((bNumber>-1)&&(bNumber<28)){
     setBlock(bNumber,bColour);
-
     FastLED.show();
     server.send(200, "text/plain", "Set");
   }
   else {  server.send(200, "text/plain", "Failed");}
-
   Serial.print("handleBlock end");
 }
 
@@ -276,7 +411,6 @@ void handleRoot() {
 
 
 void handleCorner(){
-
   int bNumber=-1;
   CRGB bColour;
   //Serial.print("handleBlock called");
@@ -297,7 +431,6 @@ void handleCorner(){
       //Serial.print("blue");
       bColour.b=server.arg(i).toInt();
     }  
-
   }
   //  Serial.print("handleBlock middle");
   if ((bNumber>-1)&&(bNumber<28)){
@@ -307,37 +440,39 @@ void handleCorner(){
     server.send(200, "text/plain", "Set");
   }
   else {  server.send(200, "text/plain", "Failed");}
-
-
 }
+
+ void set_crawl_from_default(int n, uint32_t phase, uint32_t pattern) {
+   set_crawl(n, phase, pattern,
+	     slot[n].main_colour,
+	     slot[n].main_flash_colour);
+ }
+
+void set_crawl(int n, uint32_t phase, uint32_t pattern,
+		CRGB in1, CRGB in2) {
+  int len=32-__builtin_clz(pattern);
+  for (uint32_t i=0;i<15;i++){
+    bool thisc=1&(pattern>>((i+(phase>>8))%len)),
+         nextc=1&(pattern>>((i+(phase>>8)+1)%len));
+    CRGB c1 = thisc ? in2 : in1,
+      c2 = nextc ? in2 : in1;
+    leds[i+2+19*n]=gamma_apply(blend(c1, c2, phase&255));
+  }
+}  
 
 void setBlock(int number, CRGB colour){
-
-  int start=19*number;
-  for (int i=0;i<15;i++){
-
-    leds[i+2+start]=colour;
-  }
-
+  fill_solid(&leds[19*number+2], 15, gamma_apply(colour));
 }
 
-
 void setCorner(int number, CRGB colour){
-  int start=19*number;
-
-  for (int i=0;i<2;i++){
-
-    leds[i+start]=colour;
-    leds[i+17+start]=colour;
-  }
-
+  fill_solid(&leds[19*number], 2, gamma_apply(colour));
+  fill_solid(&leds[19*number+17], 2, gamma_apply(colour));
 }
 
 void handleSwitch(){
-
   String returnString="";
   for (int i=0;i<28;i++){
-    returnString+=switchState[i];    
+    returnString+=!!(switches>>i);
   }
   server.send(200, "text/plain", returnString);
 }
