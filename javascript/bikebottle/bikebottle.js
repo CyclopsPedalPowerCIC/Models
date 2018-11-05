eecf3ff053cced85e13a83ac195f1eece217c1e4
Tom Hargreaves <hex@sphere.chronosempire.org.uk>
2018-11-05 16:07:58 +0000

    bikebottle

---
+++ javascript/bikebottle/bikebottle.js
@@ -0,0 +1,36 @@
+function gebi(id) { return document.getElementById(id); }
+var q=gebi("2");
+var eta=gebi("eta");
+var str='';
+var made=0;
+var lastmade=0;
+var total=650;
+var timedelta = 1;
+function add() {
+    made += gebi("speed").value/1000;
+    var str;
+    for (var i=0; i<4; i++) {
+	str = made.toFixed(i);
+	if (str.length == 5) break;
+    }
+    q.innerHTML = str;
+    
+}
+function estimate() {
+    var delta = made-lastmade;
+    if (delta) {
+	lastmade = made;
+	var needed = total-made;
+	var time=(needed/delta)*timedelta;
+	var percent=(made/total*100).toFixed(2)+"%";
+	var hours=(time/3600)|0, minutes=((time/60)%60)|0, seconds = (time%60)|0;
+	etastr = `${hours} hours ${minutes} minutes ${seconds} seconds`;
+    } else {
+	etastr = "Forever.  Get pedalling!";
+    }
+    eta.innerHTML = etastr;
+    gebi("debug").innerHTML = `${percent} done ${needed} delta=${delta} time=${time}`;
+}
+setInterval(add,50);
+
+setInterval(estimate,timedelta*1000);
